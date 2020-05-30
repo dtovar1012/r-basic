188bd5abd50a7f5848b50b422851658aaf9ad247
dtovar1012 <65476939+dtovar1012@users.noreply.github.com>
2020-05-29 20:19:29 -0500

    Update EjerciciosT1.R

--- ejercicios/EjerciciosT1.R
+++ ejercicios/EjerciciosT1.R
@@ -13,3 +13,16 @@ opBasic = function(a,b){
   print(paste(sprintf("%i : %i = ",b,a),b%/%a))
   print(paste("con resto ",b%%a))
 }
+
+opBasic(5,4)
+i <- 9
+sprintf("El cuadrado de %d es %d", i, i^2)
+
+Calculadora_seg = function (x){
+  
+  print("segundos / Minutos")
+  print( x /60 )}
+  
+
+Calculadora_seg(250000000)  
+
